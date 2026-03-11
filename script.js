6e23d389a89c7c2acaf31c6f6e2f6de8c0a213d6
Dark-Vader39 <rustagikavyam@gmail.com>
2026-03-11 10:44:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var choice = prompt("Please enter a One-digit Number in The given blank")
+<p id="even" ></p>
+<p id="odd"></p>
+if(a == 0, 2, 4, 6, 8,) {
+  even = "This is an Even One-Digit number"
+} else {
+  odd = "This is an Odd One-Digit Number"
+}
